d335d6ba8a6bbd3a07b6aa191f7319fd95462e09
Samiullah <samiullah954015@gmail.com>
2026-05-06 19:57:04 +0500

    t

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// This is the entry point of the React application. It imports the necessary modules and renders the App component into the DOM.
   import { createRoot } from "react-dom/client";
   import App from "./app/App.jsx";
   import "./styles/index.css";
